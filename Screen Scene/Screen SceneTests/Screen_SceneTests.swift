f5ac33cb7f4cbf30b35560ad8518113d5821f80a
mgrabovac22 <grabovacmarin000gmail.com>
2024-11-13 14:32:53 +0100

    Created project in xCode

---
+++ Screen Scene/Screen SceneTests/Screen_SceneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_SceneTests.swift
+//  Screen SceneTests
+//
+//  Created by RAMPU on 12.11.2024..
+//
+
+import Testing
+@testable import Screen_Scene
+
+struct Screen_SceneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Screen Scene/Screen SceneUITests/Screen_SceneUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Screen_SceneUITestsLaunchTests.swift
+//  Screen SceneUITests
+//
+//  Created by RAMPU on 12.11.2024..
+//
+
+import XCTest
+
+final class Screen_SceneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
